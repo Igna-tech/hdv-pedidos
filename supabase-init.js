5da0fb9556bb4d6d7498d605a2082c3b6e83237c
Igna-tech <diazignacio42@gmail.com>
2026-03-11 17:20:26 -0300

    act

--- supabase-init.js
+++ supabase-init.js
@@ -8,4 +8,10 @@ const SUPABASE_URL = 'https://ngtoshttgnfgbiurnrix.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ndG9zaHR0Z25mZ2JpdXJucml4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxODAwNjMsImV4cCI6MjA4ODc1NjA2M30.x_s34j_YOsMgxAhFPOUvGTIRaJoRRvOUfDqQGHNZdcM';
 
 const { createClient } = supabase;
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+    auth: {
+        storage: sessionStorage,   // Cada pestaña mantiene su propia sesion
+        autoRefreshToken: true,
+        persistSession: true
+    }
+});
